57d83339dd4922797e76983d1d3cf5fda132fc44
Varis Nijat <vnijat93@gmail.com>
2022-06-20 20:03:27 -0400

    DEV Latest update

--- app/main/views.py
+++ app/main/views.py
@@ -14,6 +14,11 @@ def index():
 @main.route('/submit', methods=['POST'])
 def submit():
     results, column_count = process_instructions(request.form["code"])
+    results_for, column_count_for = process_instructions(
+        request.form["code"],
+        True
+    )
     return render_template(
-        'results.html', results=results, column_count=column_count
+        'results.html', results=results, column_count=column_count,
+        results_for=results_for, column_count_for=column_count_for
         )
--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ from typing import List
 logging.basicConfig(level=logging.DEBUG)
 
 
-def process_instructions(instructions: str):
+def process_instructions(instructions: str, forwarding=False):
 
     global REGISTERS
     REGISTERS = {}
@@ -24,7 +24,7 @@ def process_instructions(instructions: str):
         result = {}
         result["instruction"] = instruction
 
-        cur_seq = generate_sequence(instruction, pre_seq)
+        cur_seq = generate_sequence(instruction, pre_seq, forwarding)
 
         logging.info(f"current: {cur_seq}. previous: {pre_seq}")
         pre_seq = cur_seq
@@ -41,7 +41,7 @@ def process_instructions(instructions: str):
     return results, column_count
 
 
-def generate_sequence(cur_ins, pre_seq):
+def generate_sequence(cur_ins, pre_seq, forwarding):
 
     sequence = []
     opcode = cur_ins.split(' ')[0]
@@ -49,16 +49,29 @@ def generate_sequence(cur_ins, pre_seq):
     src_reg_1 = cur_ins.split(' ')[2]
     src_reg_2 = ''
 
+    if not forwarding:
+        cycles = 3
+    else:
+        if opcode == 'lw':
+            cycles = 2
+        else:
+            cycles = 0
+
     if opcode in THREE_OPERANDS:
         src_reg_2 = cur_ins.split(' ')[3]
 
     if len(pre_seq) == 0:
-        add_register(dest_reg)
+        add_register(dest_reg, cycles)
+        update_registers()
         return TIMING_SEQUENCE
 
     else:
         sequence = [''] * (pre_seq.index('ID')) + ["IF"]
-        stall_cycles = get_stall_cycles(src_reg_1, src_reg_2)
+
+        if opcode == 'sw':
+            stall_cycles = get_stall_cycles(src_reg_1, src_reg_2, dest_reg)
+        else:
+            stall_cycles = get_stall_cycles(src_reg_1, src_reg_2)
         sequence += ['S'] * stall_cycles
         sequence += ["ID", "EX", "ME", "WB"]
 
@@ -66,14 +79,23 @@ def generate_sequence(cur_ins, pre_seq):
             update_registers()
             logging.info(f"regiters after updating {REGISTERS}")
 
-    add_register(dest_reg)
+    add_register(dest_reg, cycles)
     update_registers()
     return sequence
 
 
-def get_stall_cycles(src_reg_1: str, src_reg_2: str):
+def get_stall_cycles(src_reg_1: str, src_reg_2: str, dest_reg=None):
 
-    if src_reg_1.startswith('$'):
+    cycle_0 = REGISTERS.get(dest_reg, 0)
+
+    if src_reg_1.startswith('$') or src_reg_1.endswith(')'):
+
+        if src_reg_1.endswith(')'):
+            start = src_reg_1.find('(')
+            end = src_reg_1.find(')')
+
+            src_reg_1 = src_reg_1[start+1:end]
+
         # This is a register
         cycle_1: int = REGISTERS.get(src_reg_1, 0)
         print(src_reg_1, cycle_1)
@@ -82,14 +104,16 @@ def get_stall_cycles(src_reg_1: str, src_reg_2: str):
         # This is a register
         cycle_2: int = REGISTERS.get(src_reg_2, 0)
         print(src_reg_2, cycle_2)
+    elif src_reg_2 == '':
+        cycle_2 = 0
 
-    return max(cycle_1, cycle_2)
+    return max(cycle_0, cycle_1, cycle_2)
 
 
-def add_register(reg):
+def add_register(reg, cycles):
 
     logging.info(f"adding {reg} to the registers {REGISTERS}")
-    REGISTERS[reg] = 2
+    REGISTERS[reg] = cycles
     return
 
 
